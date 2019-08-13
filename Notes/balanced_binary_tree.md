5a10b937e6e4accb9422fa67f5942ad67a13bc2c
shushiej <shushruthjoshi@gmail.com>
2019-08-14 07:04:44 +1200

    108. Convert sorted array to binary search tree

---
+++ Notes/balanced_binary_tree.md
@@ -0,0 +1,10 @@
+## Test if a Binary Tree is Balanced.
+
+If a BST is balanced, then at every node the **absolute difference** between the left subtree and the right subtree cannot be more than 1.
+
+```
+|left_subtree - right_subtree| <= 1
+```
+
+### Acknowledgements
+Check out this wonderful video by `Back to Back SWE` on the [Balanced Binary Tree](https://www.youtube.com/watch?v=LU4fGD-fgJQ), with code samples. 
---
+++ sorted_array_to_BST/solution.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if(nums == None or len(nums) == 0):
+            return None
+        
+        # Enter recursive call to build up the tree.
+        return self.constructBST(nums, 0, len(nums) - 1)
+    
+    # Recursive function which creates a new root of the tree based on the left and right pointers.
+    def constructBST(self, nums, left, right):
+        if(left > right):
+            return None
+        
+        # find the middle element of the list
+        mid = left + (right - left) / 2
+        
+        #initialise the root of the tree/subtree
+        current = TreeNode(nums[mid])
+        
+        #recursively build the left and right nodes of the current node.
+        current.left = self.constructBST(nums, left, mid - 1)
+        current.right = self.constructBST(nums, mid + 1, right)
+        
+        return current
