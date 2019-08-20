574fc914d6cbfd82d1cc2a87ad0c7f38ca630271
shushiej <shushruthjoshi@gmail.com>
2019-08-21 05:48:15 +1200

    122. Buy and Sell Stock II

---
+++ buy_and_sell_stock_ii/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) == 0:
+            return 0
+        
+        profit = 0
+        
+        for i in range(1, len(prices)):
+            diff = prices[i] - prices[i - 1]
+            if(diff > 0):
+                profit += diff
+        return profit
+        
+        
